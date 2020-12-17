dd262a2b492d6b6f526c522381a38d0f610203e1
Kevin Quesada <kevinque01@gmail.com>
2020-12-16 22:35:16 -0600

    First Commit

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import api from './api';
+import DealList from './components/DealList';
 
 export default function App() {
+  const [deals, setDeals] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const deals = await api.fetchInitialDeals();
+      setDeals(deals);
+    })();   
+
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {
+        deals.length > 0 ? (
+          <DealList deals={deals} />
+        ) : (
+          <Text style={styles.header}>BigSale App!</Text>
+        )
+      }     
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +32,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    fontSize: 40,
+  }
 });
---
+++ components/DealList.js
@@ -0,0 +1,23 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default DealList = ({deals}) => {
+
+    return (
+        <View style={styles.list}>
+            <FlatList data={deals} rendeItem={({item}) => 
+                <Text>{item.title}</Text>
+            } keyExtractor={item => item.key}/>
+        </View>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    list: {
+        backgroundColor: '#eee',
+        flex: 1,
+        width: '100%',
+        paddingTop: 50,
+    },
+});
